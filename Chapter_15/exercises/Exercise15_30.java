92344267b189830e4bdef15afe909147ef45a77f
suwin <suwin@su-mbp-2018.lan>
2023-10-24 12:39:03 -0400

    today challenge: exercise 15.30

--- Chapter_15/exercises/Exercise15_30.java
+++ Chapter_15/exercises/Exercise15_30.java
@@ -1,5 +1,64 @@
 package exercises;
+//Written by Su Win
 
-public class Exercise15_30 {
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.util.Duration;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.image.*;
+import javafx.animation.*;
+import javafx.event.*;
+
+//Slide Show
+/* Slides are displayed in order and automatically displays them repeatedly
+ * Each slide is shown for 2 seconds
+ * When the last slide finishes, the first slide is redisplayed.
+ * Click to pause if the animation is currently playing.
+ * Click to resume if the animation is currently paused.
+ */
+public class Exercise15_30 extends Application{
+	
+	private ImageView slideView = new ImageView();
+	private int slideIndex = 0, totalImages = 24;
+	
+	@Override
+	public void start(Stage primaryStage) {
+
+		StackPane stackPane = new StackPane();
+
+		//handler for changing slides
+		EventHandler<ActionEvent> eventHandler = e ->{
+				slideView.setImage((new Image("resources/slide"+slideIndex+".jpg")));
+				stackPane.getChildren().clear();
+				stackPane.getChildren().add(slideView);
+				slideIndex = (slideIndex+1) % totalImages;
+		};
+		
+		//animation for alternating slides
+		Timeline animation = new Timeline(new KeyFrame(Duration.seconds(2), eventHandler));
+		animation.setCycleCount(Timeline.INDEFINITE); //run animation indefinitely
+		animation.play(); //start playing animation
+		
+		//When user click, get the animation status and if the animation is pause, resume the animation.
+		//If the animation is play, pause the animation.
+		slideView.setOnMouseClicked(e -> {
+			if(animation.getStatus() == Animation.Status.PAUSED) {
+				animation.play();
+			}else {
+				animation.pause();
+			}
+		});
+		
+		Scene scene = new Scene(stackPane, 800, 600);
+		primaryStage.setTitle("Slide Show");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
